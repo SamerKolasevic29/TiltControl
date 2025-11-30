2442c7f5c56f2801b95b1cdbb6ada577228ab253
SamerKolasevic29 <samer.kolasevic.24@size.ba>
2025-11-30 16:02:55 +0100

    Initial commit and making MAUI project blank

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TiltControl
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TiltControl
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
